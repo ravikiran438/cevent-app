107b6e92427e4b902c36d8770945c9dc2148c3db
Ravi K <ravikiran438@gmail.com>
2014-02-18 21:11:47 -0500

    Some clean up

--- src/main/java/com/mycompany/cevent/config/Constants.java
+++ src/main/java/com/mycompany/cevent/config/Constants.java
@@ -11,5 +11,7 @@ public final class Constants {
     public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
 
     public static final String SPRING_PROFILE_PRODUCTION = "prod";
+    
+    public static final String SPRING_PROFILE_OPENSHIFT = "openshift";
 
 }
